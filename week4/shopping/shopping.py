8f757e7efd692cc3fc2e6fec4148ffcf2f327116
Mariam Khokhiashvili <142008382+MariamKhoKh@users.noreply.github.com>
2025-04-14 22:52:50 +0400

    remove extra import

--- week4/shopping/shopping.py
+++ week4/shopping/shopping.py
@@ -59,8 +59,6 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    import calendar
-
     evidence = []
     labels = []
 
